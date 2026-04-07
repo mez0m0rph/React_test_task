a9859f4c12770726ee3712c795b5f0444ba39a38
Viktor <a0000@MacBook-Air-0000.local>
2026-04-07 17:31:31 +0700

    изменил поле ввода + кнопки

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,10 +1,27 @@
-function Controls({ onAdd, onRemove, onEdit, onReset}) {
+function Controls({
+    inputValue,
+    onInputChange,
+    onAdd,
+    onRemove,
+    onEdit,
+    onReset,
+}) {
     return (
-        <div style={{ marginBottom: "20px", display: "flex", gap: "10px", flexWrap: "wrap"}}>
-            <button onClick={onAdd}>Add Child</button>
-            <button onClick={onRemove}>Remove Child</button>
-            <button onClick={onEdit}>Edit selected</button>
-            <button onClick={onReset}>Reset</button>
+        <div className="controls">
+            <input 
+                type="text"
+                placeholder="введите название узла"
+                value={inputValue}
+                onChange={(e) => onInputChange(e.target.value)}
+                className="node-input"
+            />
+
+            <div className="button-group">
+                <button onClick={onAdd}>Add Child</button>
+                <button onClick={onEdit}>Apply Name</button>
+                <button onClick={onRemove}>Remove Selected</button>
+                <button onClick={onReset}>Reset</button>
+            </div>
         </div>
     );
 }
